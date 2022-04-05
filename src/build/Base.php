e6e63117e411f35d45498feb8fbfa0d47ec0160f
willphp <94844305+willphp@users.noreply.github.com>
2022-04-05 20:47:52 +0800

    Update Base.php

--- src/build/Base.php
+++ src/build/Base.php
@@ -1,4 +1,12 @@
 <?php
+/*--------------------------------------------------------------------------
+ | Software: [WillPHP framework]
+ | Site: www.113344.com
+ |--------------------------------------------------------------------------
+ | Author: no-mind <24203741@qq.com>
+ | WeChat: www113344
+ | Copyright (c) 2020-2022, www.113344.com. All Rights Reserved.
+ |-------------------------------------------------------------------------*/
 namespace willphp\request\build;
 use willphp\config\Config;
 use willphp\cookie\Cookie;
